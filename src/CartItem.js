9e6f11b4045d38d6526c4ea8851942246a219e86
aBHi-45 <abhi287786@gmail.com>
2021-12-29 16:08:46 +0530

    call functions from child to parent

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,36 +2,36 @@ import styles from "./index.css";
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            title : 'Phone',
-            qty: 1,
-            img: ""
-        }
-    }
-    IncreaseQuantity = ()=>{
-        this.setState((prevState) => {
-            return{
-                qty: prevState.qty +1
-            }
-        }); 
-    }
-    DecreaseQuantity = () =>{
-        if(this.state.qty > 0){
-            this.setState((prevState) => {
-                return{
-                    qty: prevState.qty -1
-                }
-            });
-        }
-    }
-    DeleteQuantity = () =>{
-        this.setState({
-            qty: 0
-        });
-    }
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         price : 999,
+    //         title : 'Phone',
+    //         qty: 1,
+    //         img: ""
+    //     }
+    // }
+    // IncreaseQuantity = ()=>{
+    //     this.setState((prevState) => {
+    //         return{
+    //             qty: prevState.qty +1
+    //         }
+    //     }); 
+    // }
+    // DecreaseQuantity = () =>{
+    //     if(this.state.qty > 0){
+    //         this.setState((prevState) => {
+    //             return{
+    //                 qty: prevState.qty -1
+    //             }
+    //         });
+    //     }
+    // }
+    // DeleteQuantity = () =>{
+    //     this.setState({
+    //         qty: 0
+    //     });
+    // }
     render (){
         const {price, title, qty} = this.props.product;
         return (
@@ -44,8 +44,16 @@ class CartItem extends React.Component {
                     <div>Rs {price} </div>
                     <div> {qty} </div>
                     <div className="cart-item-actions">
-                        <img alt="Increase" src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665629.png?token=exp=1640700228~hmac=ced93ff4fe9b29e4388c7028f1580c4b" className="action-icons" onClick={this.IncreaseQuantity} />
-                        <img alt="Decrease" src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665663.png?token=exp=1640700158~hmac=22179246ea7964fc3628e45aabf12082" className="action-icons" onClick={this.DecreaseQuantity} />
+                        <img 
+                        alt="Increase" 
+                        src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665629.png?token=exp=1640700228~hmac=ced93ff4fe9b29e4388c7028f1580c4b" 
+                        className="action-icons" 
+                        onClick={() => this.props.onIncreaseQuantity(this.props.product)} />
+                        <img 
+                        alt="Decrease" 
+                        src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665663.png?token=exp=1640700158~hmac=22179246ea7964fc3628e45aabf12082" 
+                        className="action-icons" 
+                        onClick={() => this.props.onDecreaseQuantity(this.props.product)} />
                         <img alt="Delete" src="https://cdn-icons.flaticon.com/png/512/2782/premium/2782988.png?token=exp=1640700407~hmac=1bdda28a489878c8319bee2caf5ea342" className="action-icons" onClick={this.DeleteQuantity} />
                     </div>
                 </div>
